06b99abd0c99cf7a0ddd93b7d992f7d7ce52a5bb
nguyenbalinh1401 <linhnbse161930@fpt.edu.vn>
2025-09-30 13:56:35 +0700

    push fix

--- TripioBE/Trippio-main/src/Trippio.Data/Service/SmsService.cs
+++ TripioBE/Trippio-main/src/Trippio.Data/Service/SmsService.cs
@@ -1,35 +1,35 @@
-﻿using Microsoft.Extensions.Options;
-using Trippio.Core.ConfigOptions;
-using Trippio.Core.Services;
-using Twilio;
-using Twilio.Rest.Api.V2010.Account;
-namespace Trippio.Data.Service
-{
-    public class SmsService : ISmsService
-    {
-        private readonly TwilioSettings _twilioSettings;
+﻿//using Microsoft.Extensions.Options;
+//using Trippio.Core.ConfigOptions;
+//using Trippio.Core.Services;
+//using Twilio;
+//using Twilio.Rest.Api.V2010.Account;
+//namespace Trippio.Data.Service
+//{
+//    public class SmsService : ISmsService
+//    {
+//        private readonly TwilioSettings _twilioSettings;
 
-        public SmsService(IOptions<TwilioSettings> twilioSettings)
-        {
-            _twilioSettings = twilioSettings.Value;
-            TwilioClient.Init(_twilioSettings.AccountSid, _twilioSettings.AuthToken);
-        }
+//        public SmsService(IOptions<TwilioSettings> twilioSettings)
+//        {
+//            _twilioSettings = twilioSettings.Value;
+//            TwilioClient.Init(_twilioSettings.AccountSid, _twilioSettings.AuthToken);
+//        }
 
-        public async Task SendSmsAsync(string phoneNumber, string message)
-        {
-            var msg = await MessageResource.CreateAsync(
-                body: message,
-                from: new Twilio.Types.PhoneNumber(_twilioSettings.PhoneNumber),
-                to: new Twilio.Types.PhoneNumber(phoneNumber)
-            );
+//        public async Task SendSmsAsync(string phoneNumber, string message)
+//        {
+//            var msg = await MessageResource.CreateAsync(
+//                body: message,
+//                from: new Twilio.Types.PhoneNumber(_twilioSettings.PhoneNumber),
+//                to: new Twilio.Types.PhoneNumber(phoneNumber)
+//            );
 
-            Console.WriteLine($"SMS sent to {phoneNumber}. SID: {msg.Sid}");
-        }
+//            Console.WriteLine($"SMS sent to {phoneNumber}. SID: {msg.Sid}");
+//        }
 
-        public async Task SendPhoneOtpAsync(string phoneNumber, string name, string otp)
-        {
-            var message = $"Xin chào {name}! Mã OTP xác thực tài khoản Trippio của bạn là: {otp}. Mã này có hiệu lực trong 5 phút.";
-            await SendSmsAsync(phoneNumber, message);
-        }
-    }
-}
+//        public async Task SendPhoneOtpAsync(string phoneNumber, string name, string otp)
+//        {
+//            var message = $"Xin chào {name}! Mã OTP xác thực tài khoản Trippio của bạn là: {otp}. Mã này có hiệu lực trong 5 phút.";
+//            await SendSmsAsync(phoneNumber, message);
+//        }
+//    }
+//}
